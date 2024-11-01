8a28b7c5b5f9bbf72e376a3f1d5b8020c3e2dda8
baovkb <21521860@gm.uit.edu.vn>
2024-11-01 07:50:56 +0700

    add dialog, refactor ws v-m

--- app/src/main/java/com/vkbao/remotemm/views/fragments/ConnectFragment.java
+++ app/src/main/java/com/vkbao/remotemm/views/fragments/ConnectFragment.java
@@ -1,15 +1,19 @@
 package com.vkbao.remotemm.views.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +23,14 @@ import com.vkbao.remotemm.R;
 import com.vkbao.remotemm.databinding.FragmentConnectBinding;
 import com.vkbao.remotemm.viewmodel.WebsocketViewModel;
 import com.vkbao.remotemm.views.activities.MainActivity;
+import com.vkbao.remotemm.views.activities.ScanQrActivity;
 
 
 public class ConnectFragment extends Fragment {
     private FragmentConnectBinding binding;
     private WebsocketViewModel websocketViewModel;
     private String url;
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
 
     public ConnectFragment() {
         // Required empty public constructor
@@ -33,6 +39,22 @@ public class ConnectFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            String url = data.getStringExtra("QR_CODE_VALUE");
+                            if (url != null)
+                                websocketViewModel.connect(url);
+                        }
+                    }
+                });
+
     }
 
     @Override
@@ -93,6 +115,11 @@ public class ConnectFragment extends Fragment {
         binding.connectBtn.setOnClickListener(view -> {
             url = binding.addressInput.getText().toString();
             websocketViewModel.connect(url);
+        });
+
+        binding.qrBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), ScanQrActivity.class);
+            someActivityResultLauncher.launch(intent);
         });
 
         binding.progressConnect.setVisibility(View.GONE);
